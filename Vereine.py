f2ba9a2eb783224d5d9c3e1d215c4daca9a457ef
Markus Konietzka <j0km0023@unibw.de>
2023-03-01 17:49:48 +0100

    class Vereine added

---
+++ Vereine.py
@@ -0,0 +1,37 @@
+class Vereine:
+
+    def __setitem__(self, key, value):
+        self.vereine[key] = value
+
+    def vereine_hinzufuegen(self, daten):
+
+        aktueller_verein = 1
+        vereinsname = 2
+        von = 3
+        bis = 4
+
+        for i in range(0, len(daten)):
+
+            if (daten[von] == daten[bis]):
+                self.vereine[daten[von]] = daten[vereinsname]
+            else:
+                for k in range(daten[von], daten[bis]):
+                    self.vereine[k] = daten[vereinsname]
+
+            vereinsname = vereinsname+3
+            von = von+3
+            bis = bis+3
+
+            if (bis >= len(daten)):
+                for index in range(daten[von], 2023):
+                    self.vereine[index] = daten[vereinsname]
+                self.vereine[2023] = daten[aktueller_verein]
+                break
+
+    def __init__(self, liste):
+        self.vereine = {}
+        self.vereine_hinzufuegen(liste)
+
+    def __str__(self):
+        return str(self.vereine)
+
